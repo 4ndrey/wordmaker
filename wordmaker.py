39b4fe4c4d7d7c6c491bbe21d16601c808e19d3c
Andrey Toropchin <4ndrey@gmail.com>
2012-11-03 13:53:58 +0600

    Fixed code identation

--- wordmaker.py
+++ wordmaker.py
@@ -4,34 +4,34 @@ import os
 import sys
 
 def containsAll(str, set):
-	istr = str
-	iset = set
-	while len(iset) > 0:
-		c = iset[0]
-		if c not in istr: 
-			return 0
-		else:
-			iset = iset.replace(c, "", 1)
-			istr = istr.replace(c, "", 1)		    	
-   	return 1
+    istr = str
+    iset = set
+    while len(iset) > 0:
+        c = iset[0]
+        if c not in istr: 
+            return 0
+        else:
+            iset = iset.replace(c, "", 1)
+            istr = istr.replace(c, "", 1)               
+    return 1
 
 letters = sys.argv[1]
 dictPath = "words/"
 words = []
 
 for filename in os.listdir(dictPath):
-	for line in open(dictPath + filename):
-		word = line.strip()
-		if word != "":
-			words.append(word)
+    for line in open(dictPath + filename):
+        word = line.strip()
+        if word != "":
+            words.append(word)
 
-result = []			
-	
+result = []         
+    
 for word in words:
-	if containsAll(letters, word):		
-		result.append(word)
+    if containsAll(letters, word):      
+        result.append(word)
 
 result.sort(key = len)
 
 for word in result:
-	print word
+    print word
